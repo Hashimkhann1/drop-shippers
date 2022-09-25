a4b0856f33abfdc9bdf9433a574e644cb6415163
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-09-25 12:02:04 +0500

    UI updated

--- lib/view/getstarted/get_started.dart
+++ lib/view/getstarted/get_started.dart
@@ -23,38 +23,53 @@ class _GetStartedState extends State<GetStarted> {
                   fontSize: 16.0
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                child: Card(
-                  elevation: 5.0,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Row(
-                      children: [
-                        Image(image: AssetImage('images/taxidriver.png')),
-                        SizedBox(width: 10.0,),
-                        Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Continue as Driver',),
-                              Text('Lorem ipsum dolor sit amet, consectetur\nNam dapibus ac libero id blandit.',
-                              style: TextStyle(
-                                color: Color(0xFF9B9C9E),
-                                fontSize: 11.0
-                              ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              Cards(image: 'taxidriver', titleText: 'Continue as Driver', paraText: 'Lorem ipsum dolor sit amet, consectetur\nNam dapibus ac libero id blandit.',),
+              Cards(image: 'deliverycourier' , titleText: 'Continue Sender & Receiver' , paraText: 'Lorem ipsum dolor sit amet, consectetur\nNam dapibus ac libero id blandit.',),
+              SizedBox(height: 100.0,)
             ],
           ),
         )
+    );
+  }
+}
+
+class Cards extends StatelessWidget {
+  Cards({this.image , this.paraText , this.titleText});
+
+  String? image;
+  String? titleText;
+  String? paraText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Card(
+        elevation: 5.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Row(
+            children: [
+              Image(image: AssetImage('images/$image.png')),
+              SizedBox(width: 10.0,),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(titleText.toString()),
+                    Text(paraText.toString(),
+                    style: TextStyle(
+                      color: Color(0xFF9B9C9E),
+                      fontSize: 11.0
+                    ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
